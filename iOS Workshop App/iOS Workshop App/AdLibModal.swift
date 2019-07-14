99bbb0337b4c4fe4fa0797233afa690a5b56e030
Elysia Lock <elysialock@gmail.com>
2019-07-14 13:26:10 -0500

    Added back buttons to castle pic & ad lib views to navigate home. Added labels, text field, and buttons to ad lib view. Added view for modal.
    Still TODO: 1. Use text from text boxes to output text in modal. 2. Add close button to modal. 3. Add collection view for pics. 4. Add collection of castle pics. 5. Add modal to display castle pic. 6. Write UI tests.

---
+++ iOS Workshop App/iOS Workshop App/AdLibModal.swift
@@ -0,0 +1,30 @@
+//
+//  AdLibModal.swift
+//  iOS Workshop App
+//
+//  Created by Elysia Lock on 7/14/19.
+//  Copyright © 2019 Elysia Lock. All rights reserved.
+//
+
+import UIKit
+
+class AdLibModal: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
