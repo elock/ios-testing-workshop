7b76e502bcc97cbfb35a3513c46a0eea4e92a7c4
Elysia Lock <elysialock@gmail.com>
2019-07-14 21:47:55 -0500

    Modal has working close button. Now it's possible to navigate through each view of the application.

--- iOS Workshop App/iOS Workshop App/AdLibModal.swift
+++ iOS Workshop App/iOS Workshop App/AdLibModal.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class AdLibModal: UIViewController {
 
+    @IBOutlet weak var closeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func closeModal(_ sender: UIButton) {
+        dismiss(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- iOS Workshop App/iOS Workshop App/DragonAdLib.swift
+++ iOS Workshop App/iOS Workshop App/DragonAdLib.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class DragonAdLib: UIViewController {
 
+    @IBOutlet weak var verbLabel: UILabel!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var nounLabel: UILabel!
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var openCreatedAdLibButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func openCreatedAdLib(_ sender: Any) {
     }
-    */
-
+    
 }
